a116a28b0729bdbc54404190673852fa67ba71a9
Matt Eva <matteweva@gmail.com>
2025-02-08 11:12:11 -0800

    added cors

--- freecreate/main.go
+++ freecreate/main.go
@@ -5,26 +5,18 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main(){
-	eErr := godotenv.Load()
-	ctx := context.Background()
-	if eErr != nil {
-		fmt.Println(eErr)
-	}
-	r := gin.Default()
 
-  	uri := os.Getenv("MONGO_URI")
-	  fmt.Println(uri)
+
+func mongoConfig(ctx context.Context) (*mongo.Client) {
+	uri := os.Getenv("MONGO_URI")
 
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
@@ -34,15 +26,19 @@ func main(){
 	if err != nil {
 		panic(err)
 	}
-	
+
 	err = client.Ping(ctx, nil)
-	
+
 	if err != nil {
 		log.Fatalf("failed to ping MongoDB: %v", err)
 	}
 
-	fmt.Println("successfull connected to Mongo!")
+	fmt.Println("successfully connected to Mongo!")
 
+	return client
+}
+
+func pgConfig() (*sql.DB) {
 	PGUser := os.Getenv("PG_USER")
 	PGPassword := os.Getenv("PG_PASSWORD")
 	PGName := os.Getenv("PG_NAME")
@@ -53,11 +49,10 @@ func main(){
 	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=%s",
 		PGUser, PGPassword, PGHost, PGPort, PGName, pgSSL)
 
-		db, err := sql.Open("postgres", connStr)
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("Error opening database connection:", err)
 	}
-	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
@@ -65,13 +60,22 @@ func main(){
 	}
 
 	fmt.Println("successfully connected to Postgres")
-	
 
-	r.GET("/ping", func (c *gin.Context){
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	return db
+}
 
-	r.Run()
+
+
+func main() {
+	eErr := godotenv.Load()
+	ctx := context.Background()
+	if eErr != nil {
+		fmt.Println(eErr)
+	}
+
+	pg := pgConfig()
+
+	mongo := mongoConfig(ctx)
+
+	router(pg, mongo)
 }
---
+++ freecreate/router.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		origin := c.Request.Header.Get("Origin")
+
+		// You can configure the allowed origins here
+		frontendOrigin := os.Getenv("FRONTEND_URL")
+		if origin == frontendOrigin {
+			c.Header("Access-Control-Allow-Origin", origin)
+			c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+			c.Header("Access-Control-Expose-Headers", "Content-Length")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+
+		// Handle preflight requests (OPTIONS)
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func router(pg *sql.DB, mongo *mongo.Client) {
+	r := gin.Default()
+	r.Use(CORS())
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
